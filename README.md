c854aea2d2593145f0a737d031b82cdf03dbbc9b
Andres Garcia Alves <andres_garcia_ao@hotmail.com>
2021-05-01 18:41:43 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# gRPC Demo
+
+Just a simple demo showing the use of gRPC on .NET 5.
+
+This is the first public release of this proyect.
+If you want, please send me your feedback about this app: andres.garcia.alves@gmail.com
---
+++ gRPC.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gRPC.Service;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace gRPC.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Greeter.GreeterClient client;
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string Nombre { get; set; }
+        public string Mensaje { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            client = new Greeter.GreeterClient(channel);
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task OnPost()
+        {
+            if (string.IsNullOrEmpty(this.Nombre)) { return; }
+
+            // request
+            var helloRequest = new HelloRequest() { Name = this.Nombre };
+            var result = await this.client.SayHelloAsync(helloRequest);
+
+            // response
+            this.Mensaje = result.Message;
+        }
+    }
+}
